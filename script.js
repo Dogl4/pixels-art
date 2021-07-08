9f3229d4c67c7df9582b2859f95b69222738f7fb
Dogl4 <doougllas@hotmail.com.br>
2021-07-08 15:45:20 -0300

    Adicione à página uma paleta de quatro cores distintas.

---
+++ script.js
@@ -0,0 +1,26 @@
+window.onload = () => {
+  createPaletteLine(); // 2
+
+};
+
+// Constantes globais;
+const palette = document.querySelectorAll('#color-palette .tr');
+
+// 2 Adicione à página uma paleta de quatro cores distintas.
+function corAleatoria() {
+  const option = '0123456789ABCDEF';
+  let hex = '';
+  do {
+    hex += option[(Math.floor(Math.random() * 16)).toString()];
+  } while (hex.length !== 6 || hex === 'FFFFFF');
+  return `#${hex}`;
+}
+
+function createPaletteLine() {
+  for (let i = 0; i < 4; i += 1) {
+    const paletteColor = document.createElement('div');
+    paletteColor.className = 'color td';
+    paletteColor.style.backgroundColor = corAleatoria();
+    palette[0].appendChild(paletteColor);
+  }
+}
